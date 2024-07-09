bd914ce0af4055e39709e62dad98dd57ba5a3b0d
Felipe Rodrigues <neath.felipe@gmail.com>
2024-07-09 13:42:28 -0300

    chore: Remove InstallmentsProvider from layout component and update walletWeb URL

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Header from "@/components/Header";
 import ClientLayout from "@/components/ClientLayout";
-import { InstallmentsProvider } from "@/context/InstallmentsContext";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,9 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <InstallmentsProvider>
-          <ClientLayout>{children}</ClientLayout>
-        </InstallmentsProvider>
+            <ClientLayout>{children}</ClientLayout>
       </body>
     </html>
   );
