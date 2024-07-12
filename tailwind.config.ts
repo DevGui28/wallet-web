66d4b1976c8e8677c660b719a4aaada3f0a5ab4e
Felipe Rodrigues - Neath <neath.felipe@gmail.com>
2024-07-11 21:29:51 -0300

    feat: Add type module to package.json and update postcss.config.mjs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,7 +10,7 @@ const config: Config = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Roboto', 'sans-serif'],
+        sans: ['Poppins', 'Roboto', 'sans-serif'],
       },
       animation: {
         'bounce-slow': 'bounce 2s infinite',
