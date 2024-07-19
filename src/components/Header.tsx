ca64dfa33ad6dd7e504e9d335c69f9d5e4dfda48
Felipe Rodrigues - Neath <neath.felipe@gmail.com>
2024-07-19 18:37:56 -0300

    feat: Update Header component styling and layout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,8 +4,8 @@ import Link from 'next/link'
 
 export default function Header() {
   return (
-    <header className="flex h-20 w-screen items-center justify-center bg-blue-600 px-10">
-      <div className="flex w-1/2 flex-col">
+    <header className="flex h-20 w-screen items-center justify-center bg-blue-600 px-5">
+      <div className="flex w-3/5 flex-col">
         <h1 className="poppins-bold text-gray-900">
           Ei, Bem vindo(a) devolta!
         </h1>
@@ -13,7 +13,7 @@ export default function Header() {
           {welcomePerson(new Date())}
         </p>
       </div>
-      <div className="flex w-1/2 items-center justify-end gap-3">
+      <div className="flex w-2/5 items-center justify-end gap-3">
         <Link href="/api/loggout">
           <button className="rounded-md bg-red-500 px-3 py-1 text-white">
             Deslogar
