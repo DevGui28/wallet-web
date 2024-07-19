2d8359975e285f21a7bb9a7ae329af661b6098dc
Felipe Rodrigues - Neath <neath.felipe@gmail.com>
2024-07-19 00:27:57 -0300

    feat: Add profile page with "Em construção" message

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,5 @@
+import { NotImplemented } from '@/components/NotImplemented'
+
+export default function Profile() {
+  return <NotImplemented />
+}
---
+++ src/components/NotImplemented.tsx
@@ -0,0 +1,7 @@
+export function NotImplemented() {
+  return (
+    <div className="-mt-20 flex h-screen items-center justify-center">
+      <h1 className="text-4xl">Em construção</h1>
+    </div>
+  )
+}
