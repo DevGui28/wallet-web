c78e9a5b7b3760947c7202736759aa217c511ff5
Felipe Rodrigues - Neath <neath.felipe@gmail.com>
2024-09-15 18:28:55 -0300

    fix: Remove token cookie on logout

--- src/app/api/loggout/route.ts
+++ src/app/api/loggout/route.ts
@@ -4,10 +4,7 @@ import { NextRequest, NextResponse } from 'next/server'
 export async function GET(request: NextRequest) {
   const response = NextResponse.redirect(new URL('/login', request.url))
 
-  response.cookies.set(cookiesName.token, '', {
-    path: '/',
-    maxAge: 0,
-  })
+  response.cookies.delete(cookiesName.token)
 
   return response
 }
