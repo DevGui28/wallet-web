cfeadbc771deddd6fe3bedffb4d4cb6a5ab6d5b6
feroddev <neath.felipe@gmail.com>
2025-02-11 22:40:43 -0300

    feat: atualizar mensagem de status na página do dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,7 @@
 function Page() {
   return (
-    <div className="flex h-full w-full flex-col items-center justify-center bg-card text-primary-foreground">
-      NOTHING
+    <div className="flex h-full w-full flex-col items-center justify-center bg-card text-3xl text-primary">
+      Em andamento
     </div>
   )
 }
