cccc13e6f6edb6c3fd5f5d21b0b0797b1ee22773
feroddev <neath.felipe@gmail.com>
2025-03-09 04:47:11 -0300

    feat: ajustar cores do tema e remover componente de contas do dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,4 +1,3 @@
-import { Account } from '../../components/app/Dashboard/Accounts'
 import { Summary } from '../../components/app/Dashboard/Summary'
 import { Card } from '../../components/ui/card'
 
@@ -6,7 +5,6 @@ function Page() {
   return (
     <div className="grid h-full w-full grid-cols-1 gap-6 lg:grid-cols-2">
       <Summary />
-      <Account />
       <Card className="flex items-center justify-center p-5">
         <h1>Dashboard</h1>
       </Card>
