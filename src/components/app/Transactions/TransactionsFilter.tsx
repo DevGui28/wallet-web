e9a87e81c877b8d1371597cb1f6b8208b8d06d8f
feroddev <neath.felipe@gmail.com>
2025-06-14 03:32:32 -0300

    refactor: remove módulo de contas e boletos e ajusta layout da tabela de transações

--- src/components/app/Transactions/TransactionsFilter.tsx
+++ src/components/app/Transactions/TransactionsFilter.tsx
@@ -101,7 +101,7 @@ export default function TransactionsFilter({
           )}
         </div>
 
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 3xl:grid-cols-5">
+        <div className="3xl:grid-cols-5 grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4">
           <div>
             <CustomSelect
               label="Tipo"
